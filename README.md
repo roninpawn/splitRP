c4adaad57795ffe1b1e3e89d69068df9f617feeb
roninpawn <roninpawn@gmail.com>
2020-11-18 18:59:05 -0500

    Multiprocessing deprecated. Single FFmpeg 2x faster!

--- README.md
+++ README.md
@@ -1,17 +1,9 @@
 # SplitRP
-**-Automated Video Analysis Tool for Speedrun Moderation** 
+Automated Video Analysis Tool for Speedrun Moderation 
 *(RTA and IGT timing supported)*
 
 ## Video Analyzer
 This branch deprecates live autosplitting and focuses entirely on video analysis.
-
-	- SplitRP now uses Multiprocessing to conduct video anaylsis.
-	    : 3x faster than any previous build.
-	    : Consumes only about 1.5Gb's while processing 1080p video.
-	    : Processes ~4.5 seconds of 1080p/60fps video per second.
-	    : Processes ~8.5 seconds of 1080p/30fps video per second.
-	    : Processes ~12.5 seconds of 720p/60fps video per second.
-	    : Processes ~15.5 seconds of 720p/30fps video per second.
 
 - **SplitRP automates timing** speedruns by analyzing the video submitted by a speedrunner.
     - It looks through each frame of an .mp4 file *(or other supported video format)*, and compares
@@ -23,6 +15,15 @@ This branch deprecates live autosplitting and focuses entirely on video analysis
 	 - It can even be used to help moderation **detect fake / spliced runs!** Drawing moderators'
 	    attention to sections of the video where certain frames pop up when they definitely 
 	    shouldn't!
+
+
+      - SplitRP now uses Multiprocessing to conduct video anaylsis.
+         : 3x faster than any previous build.
+         : Consumes only about 1.5Gb's while processing 1080p video.
+         : Processes ~4.5 seconds of 1080p/60fps video per second.
+         : Processes ~8.5 seconds of 1080p/30fps video per second.
+         : Processes ~12.5 seconds of 720p/60fps video per second.
+         : Processes ~15.5 seconds of 720p/30fps video per second.
 	    
 - The way this works is, a moderator or runner of a game creates a SplitRP pattern file to use
 	  as a sort of roadmap for timing runs of that game. Images of split-frames are placed in a
@@ -33,7 +34,7 @@ This branch deprecates live autosplitting and focuses entirely on video analysis
     - ***But it's not as simple as that!** SplitRP's pattern files can be built to support IGT timing by
 	  triggering 'pause' events that will discard any time spent on a load screen, or navigating
 	  menus before or during a run. Give SplitRP an image of the frame on which to start the run,
-	  and image of when a split should occur, and an image of what a load screen looks like -- and
+	  an image of when a split should occur, and an image of what a load screen looks like -- and
 	  you can write a pattern file that will start the run, immediately pause timing until loading
 	  ends, then drop into a cycle where it continuously looks for splits. Run a video file through
 	  SplitRP with that setup and it will simultaneously generate a list of both RTA and IGT splits,
@@ -44,7 +45,7 @@ This branch deprecates live autosplitting and focuses entirely on video analysis
   simply would not have been feasible. And without conversion to IGT, our leaderboards (yay! I'm a
   moderator now) would have been forced to remain pay-to-win -- where the runner with the beefiest
   PC has a thirty-second advantage.
-  
+
 ### Try SplitRP Today!*
 **No warranties stated or implied. Use at your own risk. Choking hazard for children under 3.
 Allow 8 to 12 weeks shipping and handling. Action jetpack sold seperately.*
---
+++ videostream.py
@@ -0,0 +1,94 @@
+import ffmpeg
+import numpy as np
+from warnings import warn
+# import cv2
+# from time import time as time
+
+
+# ----> REQUIRES ffmpeg.exe AND ffprobe.exe ADDED TO CALLING DIRECTORY <----
+
+
+class VideoStream:
+    def __init__(self, path, start=0, end=None, xywh=None):
+        self.path = path
+        self.start = start
+
+        probe = ffmpeg.probe(self.path)
+        inspect = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
+
+        rate = [float(s) for s in inspect['r_frame_rate'].split("/")]
+        self.frame_rate = rate[0] / rate[1]
+        self.end = int(inspect['nb_frames']) if end is None else end
+        self.total_frames = self.end - self.start
+        self.xywh = (0, 0, int(inspect['width']), int(inspect['height'])) if xywh is None else xywh
+
+        self._frame_bytes = self.xywh[2] * self.xywh[3] * 3
+        self._frame = []
+        self._raw = None
+        self._EOF = False
+
+        self.cur_frame = self.start
+
+    def config(self, start=None, end=None, xywh=None):
+        if start is not None: self.start = start
+        if end is not None: self.end = end
+        self.total_frames = self.end - self.start
+        if xywh is not None: self.xywh = xywh
+
+    def shape(self): return self.xywh[-2:]
+
+    def _even_test(self):
+        w, h = self.xywh[-2:]
+        if w % 2 > 0: w -= 1
+        if h % 2 > 0: h -= 1
+        out = (self.xywh[0], self.xywh[1], w, h)
+        if out != self.xywh:
+            warn("\n'width/height' VALUE IN 'xywh' IS NOT AN EVEN NUMBER\n(Forcing values to lower, even integer. "
+                 "This may cause errors in operations that rely on matched resolutions.)\n")
+
+            self.xywh = out
+
+    def open_stream(self):
+        self._even_test()
+        self._raw = (ffmpeg
+                     .input(self.path)
+                     .crop(x=self.xywh[0], y=self.xywh[1], width=self.xywh[2], height=self.xywh[3])
+                     .trim(start_frame=self.start, end_frame=self.end)
+                     .setpts('PTS-STARTPTS')
+                     .output('pipe:', format='rawvideo', pix_fmt='bgr24')
+                     .run_async(pipe_stdout=True)
+                     )
+        self._frame_bytes = self.xywh[2] * self.xywh[3] * 3
+
+    def read(self):
+        if not self._EOF:
+            byte_stream = self._raw.stdout.read(self._frame_bytes)
+            byte_arr = np.frombuffer(byte_stream, np.uint8)
+            if len(byte_arr):
+                self._frame = byte_arr.reshape([self.xywh[3], self.xywh[2], 3])
+                self.cur_frame += 1
+            else:
+                self._raw.stdout.close()
+                self._EOF = True
+
+        return self._EOF, self._frame
+
+
+# --- BASIC IMPLEMENTATION ---
+# path = '720_test.mp4'
+# crop = (277, 0, 447, 347)
+#
+# video = VideoStream(path, xywh=crop)
+# start = 10
+# end = video.total_frames - 10     # Init'ing the VideoStream without start/end configurations allows programmatic
+# video.config(start, end)          # methods using the ffprobe information spawned at creation. (not necessary)
+#
+# start = time()
+# video.open_stream()
+# while True:
+#     eof, frame = video.read()
+#     if eof: break
+#     cv2.imshow('window-name', frame)   # Import cv2 at the top to see the frames as fast as they can be drawn.
+#     cv2.waitKey(1)
+#
+# print(f"\nRead entire file in {round(time() - start, 3)} seconds.")
